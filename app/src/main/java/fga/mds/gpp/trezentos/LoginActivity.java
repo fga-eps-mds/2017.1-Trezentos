e68846a11044c5394a5886334adabc7b73dab378
Arthur Diniz <arthurbdiniz@gmail.com>
2017-03-27 17:35:44 -0300

    [Dialog] Implements alert dialog and progress dialog

--- app/src/main/java/fga/mds/gpp/trezentos/LoginActivity.java
+++ app/src/main/java/fga/mds/gpp/trezentos/LoginActivity.java
@@ -1,16 +1,23 @@
 package fga.mds.gpp.trezentos;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
+
     private UserDialog dialog = new UserDialog();
+    private String activityName = this.getClass().getSimpleName();
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         dialog.setContext(this);
+
         Button login = (Button) findViewById(R.id.buttonLogin);
         Button register = (Button) findViewById(R.id.buttonRegister);
 
@@ -27,8 +35,11 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //Implementar aqui parte da verificação do login
                 Log.d(TAG,"Button Login clicado");
+                dialog.setProgressMessage("Carregando...");
+                dialog.execute();
 
-                dialog.alertDialog("Usuario ou senha corretos");
+
+
 
             }
         });
@@ -40,11 +51,14 @@ public class LoginActivity extends AppCompatActivity {
 //              Intent intent = new Intent(LoginActivityActivity.this, RegisterActivity.class);
 //              startActivity(intent);
                 Log.d(TAG, "Button Registar clicado");
+                dialog.alert("Falha na Autenticação", "Tente novamente...");
+
+
+
             }
         });
 
     }
-
 
 
     @Override
--- app/src/main/java/fga/mds/gpp/trezentos/UserDialog.java
+++ app/src/main/java/fga/mds/gpp/trezentos/UserDialog.java
@@ -1,24 +1,51 @@
 package fga.mds.gpp.trezentos;
 
-import android.app.DialogFragment;
+
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 
 
-public class UserDialog extends DialogFragment {
+
+public class UserDialog extends AsyncTask<Void, Void, Void> {
 
     private AlertDialog.Builder alert;
     private Context context;
+    ProgressDialog asyncDialog;
 
-    public UserDialog() {
+    private String progressMessage;
+
+    //Shows an alert dialog with custom message
+    public void alert(String alertTitle, String  alertMessage){
+
+        new AlertDialog.Builder(context)
+                .setTitle(alertTitle)
+                .setMessage(alertMessage)
+                .setCancelable(false)
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //TODO
+                    }
+                }).show();
 
     }
 
+    //Creates an progres dialog with custom message
+    public void progressCreate(String progressMessage){
+        asyncDialog = new ProgressDialog(context);
+        String typeStatus;
+        asyncDialog.setMessage(progressMessage);
+        asyncDialog.show();
 
-    public void alertDialog(String message){
-        alert = new AlertDialog.Builder(context);
-        alert.setPositiveButton("Ok", null);
-        alert.create().show();
+    }
+
+    //Ends an progress dialog with custom message
+    public void progressDismiss(){
+        asyncDialog.dismiss();
+
 
     }
 
@@ -26,6 +53,31 @@ public class UserDialog extends DialogFragment {
         this.context = context;
     }
 
+    public void setProgressMessage(String progressMessage){
+        this.progressMessage = progressMessage;
 
+    }
+
+    @Override
+    protected void onPreExecute() {
+        progressCreate(progressMessage);
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        progressDismiss();
+
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 
 }
