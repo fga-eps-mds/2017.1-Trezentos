b16c7e2571473205ace44813b87847a244b4fe2e
Arthur Diniz <arthurbdiniz@gmail.com>
2017-03-27 12:46:54 -0300

    [INTENT] Set Intent moving to LoginActivity onCreate method

--- app/src/main/java/fga/mds/gpp/trezentos/MainActivity.java
+++ app/src/main/java/fga/mds/gpp/trezentos/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+
     }
 }
