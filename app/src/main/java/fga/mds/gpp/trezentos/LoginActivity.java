74fc7837bbfdf5e9246c1b052ecf8f41cd3dca79
Arthur Diniz <arthurbdiniz@gmail.com>
2017-03-27 12:49:53 -0300

    [VIEW] Setting up Login view

--- app/src/main/java/fga/mds/gpp/trezentos/LoginActivity.java
+++ app/src/main/java/fga/mds/gpp/trezentos/LoginActivity.java
@@ -5,9 +5,21 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
