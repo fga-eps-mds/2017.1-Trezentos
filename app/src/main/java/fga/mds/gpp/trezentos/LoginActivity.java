7698201ad2d70c689817d6fe887d7d753c5c60c0
Arthur Diniz <arthurbdiniz@gmail.com>
2017-03-27 12:33:11 -0300

    [CRUD] Creating LoginActivity

--- app/src/main/java/fga/mds/gpp/trezentos/LoginActivity.java
+++ app/src/main/java/fga/mds/gpp/trezentos/LoginActivity.java
@@ -1,16 +1,13 @@
 package fga.mds.gpp.trezentos;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_login);
     }
 }
