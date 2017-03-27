cb219136820df9f68c48db905eb2ec52cd022d9e
Arthur Diniz <arthurbdiniz@gmail.com>
2017-03-27 15:18:50 -0300

    [Set Up Listeners] Creating the method to listen buttons

--- app/src/main/java/fga/mds/gpp/trezentos/LoginActivity.java
+++ app/src/main/java/fga/mds/gpp/trezentos/LoginActivity.java
@@ -1,17 +1,46 @@
 package fga.mds.gpp.trezentos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
-
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login = (Button) findViewById(R.id.buttonLogin);
+        Button register = (Button) findViewById(R.id.buttonRegister);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Implementar aqui parte da verificação do login
+                Log.d(TAG,"Button Login clicado");
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Levar o usuario para a tela de cadastro
+//              Intent intent = new Intent(LoginActivityActivity.this, RegisterActivity.class);
+//              startActivity(intent);
+                Log.d(TAG, "Button Registar clicado");
+            }
+        });
+
     }
+
+
 
     @Override
     protected void onResume() {
