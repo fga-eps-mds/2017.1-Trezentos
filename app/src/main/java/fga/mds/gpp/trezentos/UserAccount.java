f1a7417b8b8d82ebdad4ff75dd0adb750faf624a
Guilherme Lacerda <guiglacerda@gmail.com>
2017-03-27 22:22:09 -0300

    [CRUD] Creates user account class

---
+++ app/src/main/java/fga/mds/gpp/trezentos/UserAccount.java
@@ -0,0 +1,46 @@
+package fga.mds.gpp.trezentos;
+
+/**
+ * Created by guilherme on 27/03/17.
+ */
+
+public class UserAccount {
+
+    private Integer idUserAccount;
+    private String user;
+    private String password;
+
+    public void UserAccount(Integer idUserAccount, String user, String password){
+        this.idUserAccount = idUserAccount;
+        this.user = user;
+        this.password = password;
+
+    }
+
+    public Integer getIdUserAccount(){
+        return idUserAccount;
+    }
+
+    public void setIdUserAccount(Integer idUserAccount){
+        this.idUserAccount = idUserAccount;
+
+    }
+
+    public String getUser(){
+        return user;
+    }
+
+    public void setUser(String user){
+        this.user = user;
+
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+
+    }
+}
