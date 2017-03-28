67e97ff03a153b310385d2453535f86130d18dc6
Arthur Diniz <arthurbdiniz@gmail.com>
2017-03-28 12:43:09 -0300

    [CRUD] Creates UserDao class

---
+++ app/src/main/java/fga/mds/gpp/trezentos/DAO/UserDao.java
@@ -0,0 +1,68 @@
+package fga.mds.gpp.trezentos.DAO;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fga.mds.gpp.trezentos.Model.UserAccount;
+
+/**
+ * Created by arthur on 28/03/17.
+ */
+
+public class UserDao {
+
+    Context context;
+    UserAccount userAccount;
+
+    public UserDao(){
+
+
+    }
+
+
+    public UserDao(Context context){
+            this.context = context;
+
+    }
+
+
+    public void update(UserAccount userAccount){
+        //This method will update the register in DB
+
+
+    }
+
+    public UserAccount findById(Integer idUserAccount){
+        //Get the cursor in database
+        //sends this cursor to
+        UserAccount userAccount = new UserAccount();
+
+        return userAccount;
+    }
+
+    public UserAccount findById(String usuario , String senha) {
+
+        UserAccount userAccount = new UserAccount();
+
+        return userAccount;
+    }
+
+    public List<UserAccount> findAll() throws Exception {
+        List<UserAccount> retorno = new ArrayList<UserAccount>();
+
+        return retorno;
+    }
+
+    private UserAccount montaUsuario() {
+
+        Integer idUserAccount = null;
+        String user = null;
+        String password = null;
+
+        return new UserAccount(idUserAccount, user, password);
+
+    }
+
+}
