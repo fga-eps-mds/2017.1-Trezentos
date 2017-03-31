1af1f6e8f49436a507e91ce358091cb6b232683a
Bruna Pinos <bruna.pinos@gmail.com>
2017-03-30 22:26:49 -0300

    [CRUD] Creates SignUp Activity

---
+++ app/src/main/java/fga/mds/gpp/trezentos/View/SignUpActivity.java
@@ -0,0 +1,15 @@
+package fga.mds.gpp.trezentos.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fga.mds.gpp.trezentos.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
