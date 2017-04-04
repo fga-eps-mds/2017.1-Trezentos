4e8956bab5acafc354f84abb2ed69c8f91621993
Arthur Diniz <arthurbdiniz@gmail.com>
2017-04-03 22:26:02 -0300

    [CRUD] Creates bottom menu bar

--- app/src/main/java/fga/mds/gpp/trezentos/View/MainActivity.java
+++ app/src/main/java/fga/mds/gpp/trezentos/View/MainActivity.java
@@ -1,9 +1,15 @@
 package fga.mds.gpp.trezentos.View;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.login.LoginManager;
@@ -11,6 +17,9 @@ import com.facebook.login.LoginManager;
 import fga.mds.gpp.trezentos.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +29,43 @@ public class MainActivity extends AppCompatActivity {
         //Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         //startActivity(intent);
 
-        if (AccessToken.getCurrentAccessToken() == null){
-            goLoginScreen();
-        }
+//        if (AccessToken.getCurrentAccessToken() == null){
+//            goLoginScreen();
+//        }
+
+
+
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_nav);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                switch (item.getItemId()) {
+                    case R.id.salas_item:
+                        Toast.makeText(MainActivity.this,"Button Salas", Toast.LENGTH_SHORT).show();
+
+                        return true;
+
+                    case R.id.usuario_item:
+                        Toast.makeText(MainActivity.this,"Button Usuario", Toast.LENGTH_SHORT).show();
+
+                        return true;
+
+                    case R.id.sobre_item:
+                        Toast.makeText(MainActivity.this,"Button Sobre", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.avaliacao_item:
+                        Toast.makeText(MainActivity.this,"Button Avaliação", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return true;
+            }
+
+        });
     }
 
     private void goLoginScreen() {
