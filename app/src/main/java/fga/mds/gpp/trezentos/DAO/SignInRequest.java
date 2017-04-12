e7986b2eb259dd048f2d88577ed5ad5ec41fced6
Weyler Almeida Gomes <weyler180@gmail.com>
2017-04-12 18:10:28 -0300

    [DAO] Creates class to do http request for login

--- app/src/main/java/fga/mds/gpp/trezentos/DAO/SignInRequest.java
+++ app/src/main/java/fga/mds/gpp/trezentos/DAO/SignInRequest.java
@@ -1,5 +1,61 @@
 package fga.mds.gpp.trezentos.DAO;
 
-public class SignInRequest {
+import android.os.AsyncTask;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import fga.mds.gpp.trezentos.Model.UserAccount;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class SignInRequest extends AsyncTask<String, String, String> {
+
+    private UserAccount user;
+    private String url = "https://trezentos-api.herokuapp.com/api/user/login";
+
+    public SignInRequest(UserAccount user){
+        this.user = user;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        OkHttpClient client = new OkHttpClient();
+
+        String urlWithParameters = getUrlWithParameters();
+
+        RequestBody body = RequestBody.create(null, "");
+        Request request = new Request.Builder()
+                .url(urlWithParameters)
+                .post(body)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private String getUrlWithParameters() {
+        Gson gson = new Gson();
+        String userJson = gson.toJson(user);
+
+        HttpUrl.Builder builder = HttpUrl.parse(url).newBuilder();
+        builder.addQueryParameter("user", userJson);
+
+        return builder.build().toString();
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+    }
 }
