663c9f256ffcfda0c33c702a67a6019af61d457e
Ana Carolina <anacarolinacsilva@outlook.com>
2017-04-17 21:51:17 -0300

    [TEST] Unit test Model UserClass

---
+++ app/src/test/java/fga/mds/gpp/trezentos/UserClassUnitTest.java
@@ -0,0 +1,62 @@
+package fga.mds.gpp.trezentos;
+
+import fga.mds.gpp.trezentos.Model.UserClass;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static java.lang.Double.valueOf;
+import static org.junit.Assert.*;
+
+
+public class UserClassUnitTest {
+
+    @Test
+    public void UserClass_AdditionTest() {
+        UserClass userClass = new UserClass();
+        Float floatAddition = new Float(10.0);
+        userClass.setAddition(floatAddition);
+        floatAddition.equals(userClass.getCutOff());
+    }
+
+    @Test
+    public void UserClass_ClassNameTest() {
+        UserClass userClass = new UserClass();
+        userClass.setClassName("nome");
+        assertEquals("nome", userClass.getClassName());
+
+    }
+
+    @Test
+    public void UserClass_IdUserAccountTest(){
+        UserClass userClass = new UserClass();
+        Integer integer = new Integer(10);
+        userClass.setIdUserAccount(integer);
+        assertEquals(integer,userClass.getIdUserAccount());
+    }
+
+    @Test
+    public void UserClass_PasswordTest(){
+        UserClass userClass = new UserClass();
+        userClass.setPassword("password");
+        assertEquals("password",userClass.getPassword());
+    }
+
+    @Test
+    public void UserClass_InstitutionTest(){
+        UserClass userClass = new UserClass();
+        userClass.setInstitution("Institution");
+        assertEquals("Institution",userClass.getInstitution());
+    }
+
+    @Test
+    public void UserClass_CutOffTest(){
+        UserClass userClass = new UserClass();
+        Float floatCutOff = new Float(10.0);
+        userClass.setCutOff(floatCutOff);
+        floatCutOff.equals(userClass.getCutOff());
+    }
+
+
+
+}
