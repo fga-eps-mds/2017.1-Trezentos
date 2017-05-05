60772bfb21878572ad08ccc53ecd71285f2bc72d
Cecilia <ceciliabessa@hotmail.com>
2017-05-04 21:31:02 -0300

    [REFACTOR] Refactor the exam class & creates validations

--- app/src/main/java/fga/mds/gpp/trezentos/Model/Exam.java
+++ app/src/main/java/fga/mds/gpp/trezentos/Model/Exam.java
@@ -1,37 +1,53 @@
 package fga.mds.gpp.trezentos.Model;
 
-
 import fga.mds.gpp.trezentos.Exception.UserException;
 
 public class Exam {
-
-
     private String nameExam;
     private String userClassName;
     private String classOwnerEmail;
 
-
-
-
     public Exam(){
-
 
     }
 
-    public Exam(String nameExam, String userClassName, String classOwnerEmail)throws UserException {
-
+    public Exam(String nameExam, String userClassName, String classOwnerEmail)
+            throws UserException {
         this.nameExam = nameExam;
         this.userClassName = userClassName;
         this.classOwnerEmail = classOwnerEmail;
     }
 
+    public void setNameExam(String nameExam)throws UserException{
+        Integer MAX_NAME_EXAM_LENGTH = 15;
+        Integer MIN_NAME_EXAM_LENGTH = 2;
+
+        if (nameExam != null && !nameExam.isEmpty()){
+            if (nameExam.length() < MIN_NAME_EXAM_LENGTH
+                    || nameExam.length() > MAX_NAME_EXAM_LENGTH){
+                throw new UserException("@string/msg_len_name_exam_error");
+            }else{
+                this.nameExam = nameExam;
+            }
+        } else{
+            throw new UserException("@string/msg_null_name_exam_error");
+        }
+    }
 
     public String getNameExam(){
         return nameExam;
     }
 
+    public void setUserClassName(String userClassName) throws UserException{
+        this.userClassName = userClassName;
+    }
+
     public String getUserClassName(){
         return userClassName;
+    }
+
+    public void setClassOwnerEmail(String classOwnerEmail)throws UserException{
+        this.classOwnerEmail = classOwnerEmail;
     }
 
     public String getClassOwnerEmail(){
@@ -39,20 +55,7 @@ public class Exam {
     }
 
     @Override
-    public String toString() {
-        return "\n"+"Prova: "+ nameExam+ "\n";
-    }
-
-
-    public void setClassOwnerEmail(String classOwnerEmail)throws UserException {
-        this.classOwnerEmail = classOwnerEmail;
-    }
-
-    public void setNameExam(String nameExam)throws UserException {
-        this.nameExam = nameExam;
-    }
-
-    public void setUserClassName(String userClassName) throws UserException{
-        this.userClassName = userClassName;
+    public String toString(){
+        return "\n"+"@string/exam_to_string"+ nameExam+ "\n";
     }
 }
