fcf0cc2bd3b04f5e64d8dcf3e8e6f9935ac72474
Ícaro Oliveira <icarooliv2@gmail.com>
2017-05-06 16:16:36 -0300

    [REFACTORING] Refactoring and applying stylesheet at View
    Signed-off-by Júlio Xavier <julioxavierr@gmail.com>
    Signed-off-by Ícaro Oliveira <icarooliv2@gmail.com>

--- app/src/main/java/fga/mds/gpp/trezentos/View/ClassActivity.java
+++ app/src/main/java/fga/mds/gpp/trezentos/View/ClassActivity.java
@@ -3,7 +3,6 @@ package fga.mds.gpp.trezentos.View;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,21 +10,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import fga.mds.gpp.trezentos.Model.UserClass;
 import fga.mds.gpp.trezentos.R;
 
-public class ClassActivity extends AppCompatActivity {
+public class ClassActivity extends AppCompatActivity{
 
     private FloatingActionButton floatingActionButton;
     private UserClass userClass;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class);
 
@@ -50,54 +46,42 @@ public class ClassActivity extends AppCompatActivity {
             setTitle(userClass.getClassName());
         }
 
-
-
         floatingActionButton = (FloatingActionButton) findViewById(R.id.floating_btn_add_test);
-
-        floatingActionButton.setOnClickListener(new FloatingActionButton.OnClickListener() {
+        floatingActionButton.setOnClickListener(new FloatingActionButton.OnClickListener(){
             @Override
-            public void onClick(View v) {
-
-                //openDialogFragment(v);
-                //openDialogFragment(v);
-                //Toast.makeText(ClassActivity.this,"Criar Prova", Toast.LENGTH_SHORT).show();
-
+            public void onClick(View v){
             }
         });
-
-
-
     }
 
-    private void setupViewPager(ViewPager viewPager) {
+    private void setupViewPager(ViewPager viewPager){
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
         adapter.addFragment(new ExamsFragment(), "EXAMS");
         adapter.addFragment(new StudensFragment(), "STUDENTS");
-        //adapter.addFragment(new ThreeFragment(), "THREE");
         viewPager.setAdapter(adapter);
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
+    public boolean onSupportNavigateUp(){
         onBackPressed();
+
         return true;
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu){
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_class, menu);
+
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
+    public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
 
-
-        if (id == R.id.action_settings) {
-            //Toast.makeText(ClassActivity.this,"Configurações", Toast.LENGTH_SHORT).show();
+        if (id == R.id.action_settings){
             return true;
         }else if(id == R.id.action_edit_class){
 
@@ -106,32 +90,8 @@ public class ClassActivity extends AppCompatActivity {
             intentEditClass.putExtra("Class", userClassCalled);
 
             startActivity(intentEditClass);
-            //Toast.makeText(ClassActivity.this,"Editar Sala", Toast.LENGTH_SHORT).show();
-
         }
-
         return super.onOptionsItemSelected(item);
     }
-
-
-
-
-
-//    public void openDialogFragment (View view){
-//
-//        android.app.FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-//        CreateClassDialogFragment ccdf = new CreateClassDialogFragment();
-//        ccdf.show(fragmentTransaction, "dialog");
-//
-//    }
-//
-//    public void turnOffDialogFragment(){
-//        android.app.FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-//        CreateClassDialogFragment ccdf = (CreateClassDialogFragment) getFragmentManager().findFragmentByTag("dialog");
-//        if(ccdf != null){
-//            ccdf.dismiss();
-//            fragmentTransaction.remove(ccdf);
-//        }
-//    }
 
 }
--- app/src/main/java/fga/mds/gpp/trezentos/View/CreateExamDialogFragment.java
+++ app/src/main/java/fga/mds/gpp/trezentos/View/CreateExamDialogFragment.java
@@ -1,18 +1,14 @@
 package fga.mds.gpp.trezentos.View;
 
-import android.content.Context;
+
 import android.content.DialogInterface;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-
-import fga.mds.gpp.trezentos.Controller.UserClassControl;
 import fga.mds.gpp.trezentos.Controller.UserExamControl;
 import fga.mds.gpp.trezentos.Exception.UserException;
 import fga.mds.gpp.trezentos.Model.UserAccount;
@@ -20,9 +16,9 @@ import fga.mds.gpp.trezentos.Model.UserClass;
 import fga.mds.gpp.trezentos.R;
 
 
-public class CreateExamDialogFragment extends DialogFragment {
+public class CreateExamDialogFragment extends DialogFragment{
 
-    public CreateExamDialogFragment() {
+    public CreateExamDialogFragment(){
     }
 
     public UserClass userClass;
@@ -31,118 +27,87 @@ public class CreateExamDialogFragment extends DialogFragment {
     public String classOwnnerEmail;
 
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
-
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+                             Bundle savedInstanceState){
         super.onCreateView(inflater, container, savedInstanceState);
-
         View view = inflater.inflate(R.layout.fragment_create_exam_dialog, container);
 
         getDialog().setTitle("Criar Sala");
-
         Button buttonOk = (Button) view.findViewById(R.id.ok_create_button);
         Button buttonCancel = (Button) view.findViewById(R.id.cancel_create_button);
 
-
         final EditText examNameField = (EditText) view.findViewById(R.id.examname);
-
 
         buttonOk.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 boolean isValid;
-
                 UserExamControl userExamControl = UserExamControl.getInstance(getActivity());
 
-
                 try {
-
                     isValid = confirmInformation(userExamControl, examNameField, userClassName, classOwnnerEmail);
-
-                } catch (Exception e) {
+                }catch (Exception e){
                     e.printStackTrace();
                     return;
                 }
 
-
-                if (isValid) {
-
+                if (isValid){
                     String examName = examNameField.getText().toString();
-
-
-
-                    try {
+                    try{
 
                         userExamControl.validateCreateExam(examName, userClassName, classOwnnerEmail);
-                    } catch (UserException e) {
+                    }catch (UserException e){
                         e.printStackTrace();
                     }
-
-
 
                     dismiss();
                 }
             }
         });
 
-        buttonCancel.setOnClickListener(new Button.OnClickListener() {
+        buttonCancel.setOnClickListener(new Button.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 dismiss();
             }
         });
-
         return (view);
     }
 
-
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
+    public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
-
     }
 
     @Override
-    public void onCancel(DialogInterface dialog) {
+    public void onCancel(DialogInterface dialog){
         super.onCancel(dialog);
     }
 
-
-    public boolean confirmInformation(UserExamControl userExamControl, EditText examNameField, String userClassName, String classOwnnerEmail) throws UserException {
-
-        String examName = examNameField.getText().toString();
-
-
+    public boolean confirmInformation(UserExamControl userExamControl, EditText examNameField,
+                                      String userClassName, String classOwnnerEmail)
+                                        throws UserException{
+    String examName = examNameField.getText().toString();
         boolean isValid = false;
 
         String errorMessage;
+        errorMessage = userExamControl.validateInformation(examName, userClassName, classOwnnerEmail);
 
-
-        errorMessage = userExamControl.validateInformation(examName, userClassName, classOwnnerEmail );
-
-        if (errorMessage.equals("Preencha todos os campos!")) {
-            examNameField.setError("Preencha todos os campos!");
-            isValid = false;
+        if (errorMessage.equals(getResources().getString(R.string.msg_empty_space_error_message))){
+            examNameField.setError(getResources().getString(R.string.msg_empty_space_error_message));
         }
-        if (errorMessage.equals("O nome da sala deve ter de 3 a 20 caracteres.")) {
+
+        if (errorMessage.equals(getResources().getString(R.string.msg_class_name_case_error_message))){
             examNameField.requestFocus();
-            examNameField.setError("O nome da sala deve ter de 3 a 20 caracteres.");
-            isValid = false;
+            examNameField.setError(getResources().getString(R.string.msg_class_name_case_error_message));
         }
 
-
-        if (errorMessage.equals("Sucesso")) {
-
+        else if (errorMessage.equals(getResources().getString(R.string.msg_class_success))){
             isValid = true;
         }
-
 
         return isValid;
     }
