5c419aaa16ff79430b6c52da5a2ce1b01c1b2d35
Bruna Pinos <bruna.pinos@gmail.com>
2017-05-06 16:47:06 -0300

    [BUG FIX] Fixes bug in ExampleInstrumentedTest
    Signed-off-by: Bruna Pinos <bruna.pinos@gmail.com>
    Signed-off-by: Guilherme Augusto <guilherme.francais@gmail.com>

--- app/src/androidTest/java/fga/mds/gpp/trezentos/View/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/fga/mds/gpp/trezentos/View/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("fga.mds.gpp.trezentos", appContext.getPackageName());
+        assertEquals("fga.mds.gpp", appContext.getPackageName());
     }
 }
