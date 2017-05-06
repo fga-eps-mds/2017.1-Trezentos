f90deb1c68a14f33a615b25a6f9df93de2d3ec41
Weyler Almeida Gomes <weyler180@gmail.com>
2017-05-06 14:00:15 -0300

    [MODEL] Add UserAccount and UserClass Tests
    Signed-off-by: Weyler Almeida Gomes <weyler180@gmail.com>
    Co-author: Carolina Ferreira <carol.mf14@gmail.com>

--- app/src/test/java/fga/mds/gpp/trezentos/Model/UserAccountTest.java
+++ app/src/test/java/fga/mds/gpp/trezentos/Model/UserAccountTest.java
@@ -13,87 +13,37 @@ import static org.junit.Assert.*;
 public class UserAccountTest {
 
     @Test
-    public void UserAccount_nameTest (){
+    public void shouldValidateName() throws UserException {
         UserAccount userAccount = new UserAccount();
-        try {
-            userAccount.setName("nome");
-        } catch (UserException e) {
-            e.printStackTrace();
-        }
-        assertEquals("nome",userAccount.getName());
+        userAccount.setName("nome");
+        assertEquals("nome", userAccount.getName());
     }
 
     @Test
-    public void TestForException (){
-        try {
-            UserAccount userAccount = new UserAccount();
-            userAccount.setEmail("email");
-            assertEquals("email", userAccount.getEmail());
-        } catch (Exception c){
-
-        }
-    }
-
-//    @Test
-//    public void UserAccount_passwordTest () {
-//        UserAccount userAccount = new UserAccount();
-//        try {
-//            userAccount.setPasswordConfirmation("password");
-//            userAccount.setPassword("password");
-//        } catch (UserException e) {
-//            e.printStackTrace();
-//        }
-//        assertEquals("password", userAccount.getPassword());
-//        assertEquals("password", userAccount.getPasswordConfirmation());
-//    }
-
-    @Test
-    public void UserAccount_NameTest (){
-        try{
-            UserAccount userAccount = new UserAccount();
-            userAccount.setName("name");
-            assertEquals("name",userAccount.getName());
-        } catch (Exception c){
-        }
+    public void shouldValidateEmail() throws UserException {
+        UserAccount userAccount = new UserAccount();
+        userAccount.setEmail("Joao123@email.com");
+        assertEquals("Joao123@email.com", userAccount.getEmail());
     }
 
     @Test
-    public void UserAccount_EmailTest() {
-        try{
-            UserAccount userAccount = new UserAccount();
-            userAccount.setEmail("email");
-            assertEquals("email", userAccount.getEmail());
-        } catch (Exception c) {
-        }
+    public void shouldValidatePasswordConfirmation() {
+        UserAccount userAccount = new UserAccount();
+        userAccount.setPasswordConfirmation("PasswordConfirmation");
+        assertEquals("PasswordConfirmation", userAccount.getPasswordConfirmation());
     }
 
     @Test
-    public void UserAccount_PasswordTest(){
-        try{
-            UserAccount userAccount = new UserAccount();
-            userAccount.setPassword("Password");
-            assertEquals("Password", userAccount.getPassword());
-        } catch (Exception c){
-        }
+    public void shouldValidateSalt() {
+        UserAccount userAccount = new UserAccount();
+        userAccount.setSalt("Salt");
+        assertEquals("Salt", userAccount.getSalt());
     }
 
     @Test
-    public void UserAccount_PasswordConfirmationTest(){
-        try{
-            UserAccount userAccount = new UserAccount();
-            userAccount.setPasswordConfirmation("PasswordConfirmation");
-            assertEquals("PasswordConfirmation", userAccount.getPasswordConfirmation());
-        } catch (Exception c){
-        }
-    }
-
-    @Test
-    public void UserAccount_SaltTest(){
-        try{
-            UserAccount userAccount = new UserAccount();
-            userAccount.setSalt("Salt");
-            assertEquals("Salt", userAccount.getSalt());
-        } catch (Exception c){
-        }
+    public void shouldValidateAuthenticatePassword() throws UserException {
+        UserAccount userAccount = new UserAccount();
+        userAccount.authenticatePassword("Password");
+        assertEquals("Password", userAccount.getPassword());
     }
 }
