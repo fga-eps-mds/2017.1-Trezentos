12431b348503e2bb86cb2724679e2427f61b522e
Bruna Pinos <bruna.pinos@gmail.com>
2017-05-06 16:42:11 -0300

    [TEST] Creates AboutOnLogin Instrumented Test
    Signed-off-by: Bruna Pinos <bruna.pinos@gmail.com>
    Signed-off-by: Guilherme Augusto <guilherme.francais@gmail.com>

---
+++ app/src/androidTest/java/fga/mds/gpp/trezentos/View/AboutOnLoginInstrumentedTest.java
@@ -0,0 +1,50 @@
+package fga.mds.gpp.trezentos.View;
+
+
+import android.content.Intent;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.widget.ListView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import fga.mds.gpp.trezentos.Exception.UserException;
+import fga.mds.gpp.trezentos.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static fga.mds.gpp.trezentos.R.id.about_on_login;
+import static fga.mds.gpp.trezentos.R.id.frame;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnit4.class)
+public class AboutOnLoginInstrumentedTest {
+
+    int NUMBER_OF_ITENS_LISTVIEW = 7;
+
+    @Rule
+    public ActivityTestRule<AboutOnLogin> aboutRule =
+            new ActivityTestRule<>(AboutOnLogin.class);
+
+    @Test
+    public void shouldCountItemsInListView() {
+
+        AboutFragment frag = (AboutFragment) aboutRule.getActivity()
+                .getSupportFragmentManager()
+                    .findFragmentById(about_on_login);
+
+        ListView listview = (ListView)
+                frag.getActivity().findViewById(R.id.about_list_view);
+
+        assertThat(listview.getAdapter().getCount(),
+                is(NUMBER_OF_ITENS_LISTVIEW));
+    }
+
+
+}
