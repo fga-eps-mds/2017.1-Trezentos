13db1a709464976c2c18a7e7574873e6aa62f24b
Ana Carolina <anacarolcarvalho23@gmail.com>
2017-05-11 18:32:18 -0300

    [UI] Changes Controller UserClassControl and layout fragments_join_class
    Signed-off-by: Ana Carolina <anacarolcarvalho23@gmail.com>
    Signed-off-by: gabrielsclimaco <gabrielsclimaco@gmail.com>

--- app/src/main/java/fga/mds/gpp/trezentos/Controller/UserClassControl.java
+++ app/src/main/java/fga/mds/gpp/trezentos/Controller/UserClassControl.java
@@ -2,6 +2,8 @@ package fga.mds.gpp.trezentos.Controller;
 
 
 import android.content.Context;
+import android.widget.Toast;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,24 +11,27 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
+import fga.mds.gpp.trezentos.DAO.AddStudentToClassRequest;
 import fga.mds.gpp.trezentos.DAO.getClassRequest;
 import fga.mds.gpp.trezentos.DAO.CreateClassPost;
+import fga.mds.gpp.trezentos.Exception.UserClassException;
 import fga.mds.gpp.trezentos.Exception.UserException;
 import fga.mds.gpp.trezentos.Model.UserClass;
+import fga.mds.gpp.trezentos.R;
 
 public class UserClassControl {
 
     private static UserClassControl instance;
     final Context context;
 
-    UserClassControl(final Context context){
+    UserClassControl(final Context context) {
         this.context = context;
     }
 
 
-    public static UserClassControl getInstance(final Context context){
+    public static UserClassControl getInstance(final Context context) {
 
-        if(instance == null){
+        if (instance == null) {
             instance = new UserClassControl(context);
         }
 
@@ -35,15 +40,15 @@ public class UserClassControl {
 
     public void validateCreateClass(String className, String institution,
                                     Float cutOff, String password, Float addition,
-                                    Integer sizeGroups, String email) throws UserException{
+                                    Integer sizeGroups, String email) throws UserException {
 
-        try{
+        try {
             UserClass userClass = new UserClass(className, institution, cutOff, password, addition, sizeGroups);
 
             CreateClassPost createClassPost = new CreateClassPost(userClass, email);
             createClassPost.execute();
 
-        }catch (UserException userException){
+        } catch (UserException userException) {
             userException.printStackTrace();
         }
 
@@ -53,10 +58,10 @@ public class UserClassControl {
 
     public String validateInformation(String className, String institution,
                                       String cutOff, String password,
-                                      String addition, String sizeGroups) throws UserException{
+                                      String addition, String sizeGroups) throws UserException {
 
         String erro;
-        try{
+        try {
             UserClass userClass = new UserClass(className, institution,
                     Float.parseFloat(cutOff), password, Float.parseFloat(addition),
                     Integer.parseInt(sizeGroups));
@@ -64,11 +69,11 @@ public class UserClassControl {
             System.out.println(userClass.getClassName());
             erro = "Sucesso";
             return erro;
-    }catch (UserException userException){
-        erro = userException.getMessage();
-        return erro;
+        } catch (UserException userException) {
+            erro = userException.getMessage();
+            return erro;
+        }
     }
-}
 
 
     public ArrayList<UserClass> getClassesFromUser(String email) {
@@ -80,17 +85,17 @@ public class UserClassControl {
         try {
             serverResponse = classRequest.execute().get();
 
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        }catch (ExecutionException e){
+        } catch (ExecutionException e) {
             e.printStackTrace();
         }
 
         ArrayList<UserClass> userClasses = new ArrayList<UserClass>();
 
-        try{
+        try {
             userClasses = getArrayList(serverResponse);
-        }catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
@@ -98,19 +103,19 @@ public class UserClassControl {
 
     }
 
-    private ArrayList<UserClass> getArrayList(String serverResponse) throws JSONException{
+    private ArrayList<UserClass> getArrayList(String serverResponse) throws JSONException {
 
         JSONArray array = null;
 
-        try{
+        try {
             array = new JSONArray(serverResponse);
-        }catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
         ArrayList<UserClass> userClasses = new ArrayList<>();
 
-        for(int i = 0; i < array.length(); i++){
+        for (int i = 0; i < array.length(); i++) {
             UserClass userClass = getUserClassFromJson(array.getJSONObject(i));
             userClasses.add(userClass);
         }
@@ -118,7 +123,7 @@ public class UserClassControl {
         return userClasses;
     }
 
-    private UserClass getUserClassFromJson(JSONObject jsonObject){
+    private UserClass getUserClassFromJson(JSONObject jsonObject) {
         UserClass userClass = new UserClass();
 
         try {
@@ -128,14 +133,31 @@ public class UserClassControl {
             userClass.setAddition(Float.parseFloat(jsonObject.getString("additionScore")));
             userClass.setPassword(jsonObject.getString("password"));
             userClass.setSizeGroups(Integer.parseInt(jsonObject.getString("numberOfStudentsPerGroup")));
+            userClass.setOwnerEmail(jsonObject.getString("ownerEmail"));
 
-        }catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
-        }catch (UserException e){
+        } catch (UserException e) {
             e.printStackTrace();
         }
 
         return userClass;
+    }
+
+    public String validateJoinClass(UserClass userClass, String password, String studentEmail) throws UserClassException, ExecutionException, InterruptedException {
+        String serverResponse = null;
+        if (!password.isEmpty()){
+            if(password.equals(userClass.getPassword())){
+                AddStudentToClassRequest addStudentToClassRequest = new AddStudentToClassRequest(userClass, studentEmail);
+                serverResponse = addStudentToClassRequest.execute().get();
+            } else {
+                throw new UserClassException("A senha errada. Por favor, digite novamente");
+            }
+        } else {
+            throw new UserClassException("A senha não pode ser vazia");
+        }
+
+        return serverResponse;
     }
 
 }
