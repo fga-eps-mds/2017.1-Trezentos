fa2c6281c7f4659446984d715880f06241e1362c
Ana Carolina <anacarolcarvalho23@gmail.com>
2017-05-11 18:27:57 -0300

    [UI] Creates AddStudentToClassRequest (DAO)
    Signed-off-by: Ana Carolina <anacarolcarvalho23@gmail.com>
    Signed-off-by: gabrielsclimaco <gabrielsclimaco@gmail.com>

--- app/src/main/java/fga/mds/gpp/trezentos/DAO/AddStudentToClassRequest.java
+++ app/src/main/java/fga/mds/gpp/trezentos/DAO/AddStudentToClassRequest.java
@@ -1,8 +1,59 @@
 package fga.mds.gpp.trezentos.DAO;
 
-/**
- * Created by ana on 11/05/17.
- */
+import android.os.AsyncTask;
+import android.util.Log;
 
-public class AddStudentToClassRequest {
+import java.io.IOException;
+
+import fga.mds.gpp.trezentos.Model.UserAccount;
+import fga.mds.gpp.trezentos.Model.UserClass;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class AddStudentToClassRequest extends AsyncTask<String, String, String>{
+    private final String student;
+    private UserClass userClass;
+
+    private String url = "https://trezentos-api.herokuapp.com/api/class/user/student";
+
+    public AddStudentToClassRequest (UserClass userClass, String student){
+        this.userClass = userClass;
+        this.student = student;
+    }
+
+    @Override
+    protected String doInBackground(String... params){
+        OkHttpClient client = new OkHttpClient();
+
+        String urlWithParameters = getUrlWithParameters();
+
+        RequestBody body = RequestBody.create(null, "");
+        Request request = new Request.Builder()
+                .url(urlWithParameters)
+                .post(body)
+                .build();
+
+        try{
+            Response response = client.newCall(request).execute();
+            return response.body().toString();
+        }catch (IOException e){
+            e.printStackTrace();
+            Log.i("LOG", "IOException in doInBackground method");
+        }
+        return null;
+    }
+
+    private String getUrlWithParameters(){
+        HttpUrl.Builder builder = HttpUrl.parse(url).newBuilder();
+
+        builder.addQueryParameter("email", userClass.getOwnerEmail());
+        builder.addQueryParameter("name", userClass.getClassName());
+        builder.addQueryParameter("student", student);
+
+        return builder.build().toString();
+    }
 }
