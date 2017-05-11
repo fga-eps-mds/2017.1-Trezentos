8fed305e6860f160a0d7c62c49b510db0f6c5c06
Ana Carolina <anacarolcarvalho23@gmail.com>
2017-05-11 17:32:03 -0300

    [UI] Creates UserClassException
    Signed-off-by: Ana Carolina <anacarolcarvalho23@gmail.com>
    Signed-off-by: gabrielsclimaco <gabrielsclimaco@gmail.com>

---
+++ app/src/main/java/fga/mds/gpp/trezentos/DAO/AddStudentToClassRequest.java
@@ -0,0 +1,8 @@
+package fga.mds.gpp.trezentos.DAO;
+
+/**
+ * Created by ana on 11/05/17.
+ */
+
+public class AddStudentToClassRequest {
+}
---
+++ app/src/main/java/fga/mds/gpp/trezentos/Exception/UserClassException.java
@@ -0,0 +1,10 @@
+package fga.mds.gpp.trezentos.Exception;
+
+
+public class UserClassException extends Exception{
+
+    public UserClassException(String message){
+        super(message);
+    }
+
+}
