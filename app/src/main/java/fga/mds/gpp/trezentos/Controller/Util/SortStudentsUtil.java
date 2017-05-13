e0c0d1ef06f7b0de6ef9a1cef8d50e037beea515
ElmarRoberto <elmarunb@gmail.com>
2017-05-13 02:22:15 -0300

    [SORT STUDENTS] Creates assortment methods
    Signed-off-by: Elmar Roberto <elmarunb@gmail.com>
    Signed-off-by: Ícaro Oliveira <icarooliv2@gmail.com>

--- app/src/main/java/fga/mds/gpp/trezentos/Controller/Util/SortStudentsUtil.java
+++ app/src/main/java/fga/mds/gpp/trezentos/Controller/Util/SortStudentsUtil.java
@@ -1,4 +1,86 @@
 package fga.mds.gpp.trezentos.Controller.Util;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SortStudentsUtil {
+    /*
+    * Purpose: Method that must be called to sort room groups
+    * @params: none
+    * @return: Map representing key(String) being the email and the value(Double) being the score
+    * */
+    public static Map<String, Double> sortGroups(){
+        Map<String, Double> map = new HashMap<>();
+
+        // Note: Size of the group and total of students coming from the database
+
+        List<Map.Entry<String, Double>> mapSortedByScore = sortByTestScore(map);
+        Map<String, Double> newMap = newMapStudents(mapSortedByScore, 3, 10);
+        return newMap;
+    }
+
+    /*
+    * Purpose: Method that creates a new student map
+    * @params: List<Map> - converted list of students from database; Integer - Room group size;
+    *          Integer - total number of students in the room
+    * @return: Map - email(key) and student group number(Double)
+    *
+    * */
+    public static Map<String, Double> newMapStudents (List<Map.Entry<String, Double>> unsortList,
+                                                      Integer groupSize, Integer totalStudents){
+        Map<String, Double> newMap = new LinkedHashMap<>();
+
+        Double it = 1.0;
+        int sent = 0;
+        Double rest = (totalStudents*1.0) % groupSize;
+        Double totalGroups = (totalStudents-rest) / groupSize;
+
+        // Assignment of group numbers
+        for (Map.Entry<String, Double> entry : unsortList) {
+            newMap.put(entry.getKey(), it);
+            if (sent == 0) {
+                it++;
+                if (it > groupSize) {
+                    sent++; it--;
+                } else {
+                    // do nothing
+                }
+            } else {
+                it--;
+                if (it == 0) {
+                    it = totalGroups;
+                } else {
+                    // do nothing
+                }
+            }
+        }
+
+        return newMap;
+    }
+
+    /*
+    * Purpose: Method that sort by student's score
+    * @params: Map - email(key) and student's score(value); Integer - Room group size;
+    *          Integer - total number of students in the room
+    * @return: List
+    * */
+    public static List<Map.Entry<String,Double>> sortByTestScore(Map<String, Double> mapStudents){
+
+        // Convert Map to list of Map
+        List<Map.Entry<String, Double>> list = new ArrayList<>(mapStudents.entrySet());
+
+        // Sort list
+        Collections.sort(list, new Comparator<Map.Entry<String, Double>>() {
+            public int compare(Map.Entry<String, Double> obj1, Map.Entry<String, Double> obj2) {
+                return obj2.getValue().compareTo(obj1.getValue());
+            }
+        });
+
+        return list;
+    }
 }
