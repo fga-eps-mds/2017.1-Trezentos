c76bf2a40a26f56c92b8c8a3af865df218a544b2
ElmarRoberto <elmarunb@gmail.com>
2017-05-13 12:45:20 -0300

    [SORT STUDENTS] Correcting methods
    Signed-off-by: Elmar Roberto <elmarunb@gmail.com>
    Signed-off-by: Ícaro Oliveira <icarooliv2@gmail.com>

--- app/src/main/java/fga/mds/gpp/trezentos/Controller/Util/SortStudentsUtil.java
+++ app/src/main/java/fga/mds/gpp/trezentos/Controller/Util/SortStudentsUtil.java
@@ -1,31 +1,30 @@
 package fga.mds.gpp.trezentos.Controller.Util;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class SortStudentsUtil {
     /*
     * Purpose: Method that must be called to sort room groups
-    * @params: none
-    * @return: Map representing key(String) being the email and the value(Double) being the score
+    * @params: Student's map from the database
+    * @return: Map representing key(String) being the email and the
+    *                           value(Double) being the groups number
     * */
-    public static Map<String, Double> sortGroups(){
-        Map<String, Double> map = new HashMap<>();
-
-        // Note: Size of the group and total of students coming from the database
+    public static Map<String, Double> sortGroups(Map<String, Double> map){
 
         List<Map.Entry<String, Double>> mapSortedByScore = sortByTestScore(map);
+        // Note: Size of the group and total of students coming from the database
         Map<String, Double> newMap = newMapStudents(mapSortedByScore, 3, 10);
+
         return newMap;
     }
 
     /*
-    * Purpose: Method that creates a new student map
+    * Purpose: Method that creates a new student map with the groups numbers
     * @params: List<Map> - converted list of students from database; Integer - Room group size;
     *          Integer - total number of students in the room
     * @return: Map - email(key) and student group number(Double)
@@ -45,7 +44,7 @@ public class SortStudentsUtil {
             newMap.put(entry.getKey(), it);
             if (sent == 0) {
                 it++;
-                if (it > groupSize) {
+                if (it > totalGroups) {
                     sent++; it--;
                 } else {
                     // do nothing
@@ -72,7 +71,7 @@ public class SortStudentsUtil {
     public static List<Map.Entry<String,Double>> sortByTestScore(Map<String, Double> mapStudents){
 
         // Convert Map to list of Map
-        List<Map.Entry<String, Double>> list = new ArrayList<>(mapStudents.entrySet());
+        List<Map.Entry<String, Double>> list = new LinkedList<>(mapStudents.entrySet());
 
         // Sort list
         Collections.sort(list, new Comparator<Map.Entry<String, Double>>() {
