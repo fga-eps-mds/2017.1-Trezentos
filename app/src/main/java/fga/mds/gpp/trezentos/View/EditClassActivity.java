ff3ffa8b22b5396b6f411dcdd9600c0e537a19be
Guilherme Augusto <guilherme.francais@gmail.com>
2017-06-19 01:23:48 -0300

    [METRICS] Improves EditClassActivity AMLOC metric
    Signed-off-by: Guilherme Augusto <guilherme.francais@gmail.com>
    Signed-off-by: Elmar Roberto <elmarunb@gmail.com>

--- app/src/main/java/fga/mds/gpp/trezentos/View/EditClassActivity.java
+++ app/src/main/java/fga/mds/gpp/trezentos/View/EditClassActivity.java
@@ -22,22 +22,40 @@ public class EditClassActivity extends AppCompatActivity{
     private EditText addition;
     private Button saveButton;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_class);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
         setTitle("Editar Sala");
 
+        initToolbar();
+        recoverLastIntent();
+        initFields();
+        initFillFields();
+
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // Update in API
+            getTextFromFields();
+            }
+
+        });
+    }
+
+    private void initToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    private void recoverLastIntent(){
         Intent intent = getIntent();
         userClass = (UserClass) intent.getSerializableExtra("Class");
+    }
 
+    private void initFields(){
         className = (EditText) findViewById(R.id.edit_text_class_name);
         instituition = (EditText) findViewById(R.id.edit_text_institution);
         classPassword = (EditText) findViewById(R.id.edit_text_class_password);
@@ -45,37 +63,30 @@ public class EditClassActivity extends AppCompatActivity{
         groupsSize = (EditText) findViewById(R.id.edit_text_size_groups);
         addition = (EditText) findViewById(R.id.edit_text_addition);
         saveButton = (Button) findViewById(R.id.button_save);
+    }
 
+    private void initFillFields(){
         className.setText(userClass.getClassName());
         instituition.setText(userClass.getInstitution());
         classPassword.setText(userClass.getPassword());
 //        cutGrade.setText((int) userClass.getCutOff());
 //        groupsSize.setText(userClass.getSizeGroups());
 //        addition.setText((int) userClass.getAddition());
+    }
 
-
-        saveButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                // Update in API
-
-                className.getText();
-                instituition.getText();
-                classPassword.getText();
-                cutGrade.getText();
-                groupsSize.getText();
-                addition.getText();
-                addition.getText();
-
-            }
-
-        });
+    private void getTextFromFields(){
+        className.getText();
+        instituition.getText();
+        classPassword.getText();
+        cutGrade.getText();
+        groupsSize.getText();
+        addition.getText();
+        addition.getText();
     }
 
     @Override
     public boolean onSupportNavigateUp(){
         onBackPressed();
-
         return true;
     }
 }
