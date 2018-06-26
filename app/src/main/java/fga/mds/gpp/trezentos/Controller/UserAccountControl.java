77b8e8198cc1ccf4ecaf59f0c9fd258461367a87
GeovanaRamos <geovannna.1998@gmail.com>
2018-06-26 09:27:10 -0300

    Corrrecting method to check if user is logged
    Changing default to false

--- app/src/main/java/fga/mds/gpp/trezentos/Controller/UserAccountControl.java
+++ app/src/main/java/fga/mds/gpp/trezentos/Controller/UserAccountControl.java
@@ -264,7 +264,7 @@ public class UserAccountControl {
 
     public boolean isLoggedUser(){
         SharedPreferences session = PreferenceManager.getDefaultSharedPreferences(context);
-        return session.getBoolean("IsUserLogged", true);
+        return session.getBoolean("IsUserLogged", false);
     }
     //End Common
 
